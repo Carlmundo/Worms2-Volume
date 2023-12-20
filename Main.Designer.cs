f162165d9c2b6e5884e644cee8ff8232aec0ce3e
Carlmundo <Carlmundo@users.noreply.github.com>
2023-12-20 10:14:48 +0000

    Disable Maximise

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -153,6 +153,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Worms2 Volume";
