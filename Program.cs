d81beab10c3840ab20c8dc360bc1ff3f79ad8161
Carl Norton <Carlmundo@users.noreply.github.com>
2024-08-11 13:24:59 +0100

    Disable Visual Styles

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace w2_volume
                 return;
             }
 
-            Application.EnableVisualStyles();
+            //Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
         }
